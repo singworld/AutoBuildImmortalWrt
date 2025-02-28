116e37db8cad3f5f7f48899c9eaae42df65cfae5
singworld <37404619+singworld@users.noreply.github.com>
2025-02-28 10:46:57 +0800

    Update build.sh

--- x86-64/24.10/build.sh
+++ x86-64/24.10/build.sh
@@ -42,6 +42,10 @@ PACKAGES="$PACKAGES openssh-sftp-server"
 PACKAGES="$PACKAGES fdisk"
 PACKAGES="$PACKAGES script-utils"
 PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
+# 自定义
+PACKAGES="$PACKAGES luci-app-sqm"
+PACKAGES="$PACKAGES luci-app-ttyd"
+PACKAGES="$PACKAGES luci-app-upnp"
 
 # 判断是否需要编译 Docker 插件
 if [ "$INCLUDE_DOCKER" = "yes" ]; then
